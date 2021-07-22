e550f1ef1d17ffe61ff766cf72c68cb6fd99d4bc
deepak.g <deepak.g@krds.fr>
2021-07-22 16:21:57 +0530

    first commit - amazing next

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+import "@styles/Home.module.css";
+
+export default function Home() {
+  return (
+    // <div className="container">
+
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
+        impedit suscipit architecto, odio inventore nostrum non neque dicta.
+        Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
+        nobis odit.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus animi
+        impedit suscipit architecto, odio inventore nostrum non neque dicta.
+        Quam magni accusantium culpa distinctio tempore iure accusamus, dolorem
+        nobis odit.
+      </p>
+    </div>
+
+    // </div>
+  );
+}
---
+++ pages/users/index.js
@@ -0,0 +1,42 @@
+export const getStaticProps = async () => {
+  try {
+    console.log("requesting....")
+    /* this code will run only in build time in production mode or compile time in dev mode */
+    const response = await fetch(
+      "https://flash-ent-cms.eu-staging.kacdn.net/pages?active_eq=true"
+    );
+    const data = await response.json();
+
+    console.log(data)
+
+    return {
+      props: { users: data },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Users = ({ users }) => {
+  return (
+    <>
+      <h1>Users list</h1>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Slug</th>
+        </tr>
+        {users.map((user) => {
+          return (
+            <tr key={user.id}>
+              <td>{user.title}</td>
+              <td>{user.slug}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </>
+  );
+};
+
+export default Users;
